ab5e2add095c508b44a61b4700a6281e166b3dec
AhSteiner <aharonsteiner@gmail.com>
2023-12-05 11:29:48 +0200

    sprint boards

--- src/AppProjects.jsx
+++ src/AppProjects.jsx
@@ -11,23 +11,14 @@ import {useAtom} from 'jotai'
 import fakeToken from './fakeToken';
 
 function AppProjects() {
-
-  
-  
+ const [userName,setUserName]= useAtom(userNameAtom)
+ const [url,setUrl] = useAtom(atomUrl)
   const [listBoards,setListBoards]=useState(null)
   const [currentProject,setCurrentProject]=useState(null)
   const [currentData, setCurrentData] = useState(null);
   const [addedBoard,setAddedBoard] = useState(null)
 
-  let serverBaseUrl;
 
-  if (process.env.NODE_ENV === 'development') {
-    
-    serverBaseUrl = 'http://localhost:3000/';
-  } else {
-    
-    serverBaseUrl = 'https://project-jerusalem-2-server.vercel.app/';
-  }
 
   useEffect(() => {
     if (addedBoard !== null) {
@@ -54,11 +45,13 @@ function AppProjects() {
   }, [currentProject]);
   
   const getDataBoards = async () => {
+    
     console.log('Wait for the data to load');
     try {
-      const response = await axios.get(`${serverBaseUrl}projects/listofprojects`);
+      const response = await axios.get(`${url}projects/listofprojects/`,{params:{userName}});
       setListBoards(response.data);
     } catch (error) {
+      if(error.response.data.auth===false){console.log('login first')}
       console.log('List not loading:', error);
     }
   };
@@ -70,7 +63,7 @@ function AppProjects() {
       return
     }
     try {
-      const response = await axios.get(`${serverBaseUrl}missions/${currentProject}`);
+      const response = await axios.get(`${url}missions/${currentProject}`);
       setCurrentData(response.data);
       
     } catch (error) {
@@ -92,11 +85,17 @@ function AppProjects() {
     }
   }
 
-  async function addBoard(name,names) {
+  async function addBoard(name,names,specs) {
     if(name===''){alert('please enter name');return}
     try {
-      const response = await axios.post(`${serverBaseUrl}projects/`, { name,names }) 
-      console.log(response.data);  
+      const response = await axios.post(`${url}projects/`, { name,names ,specs},{
+         headers: {
+        'Content-Type': 'application/json',
+        // Authorization: localStorage.getItem('token'),
+        Authorization:fakeToken //change it to the previous line when there is real token
+
+      },}) 
+      console.log(response.userInCharge);
       setAddedBoard(name)
       setCurrentProject(name)
       fetchData()
@@ -107,30 +106,29 @@ function AppProjects() {
     }
   }
 
-  async function editBoard(input,namesToAdd,namesToRemove,projectName){
+  async function editBoard(input,namesToAdd,namesToRemove,projectName,specsToAdd,specsToRemove){
     if (!input&&namesToAdd.length===0&&namesToRemove.length===0){return}
     try{
-      const response = await axios.put(`${serverBaseUrl}projects/`,{input,namesToAdd,namesToRemove,projectName})
+      const response = await axios.put(`${url}projects/`,{input,namesToAdd,namesToRemove,projectName,specsToAdd,specsToRemove,userName})
       setAddedBoard('')
       if(input!==''){
       setAddedBoard(input)
       setCurrentProject(input)}
       else{setAddedBoard(projectName);setCurrentProject(projectName)}
       fetchData()
-      console.log(response.data);
-    }catch(error){console.log('error while edit error',error);}
+    }catch(error){console.log('error while edit error',error);if(error.message.includes('403')){alert('no promissions to edit')}}
   }
 
   async function deleteBoard(projectName) {
 
       try {
         console.log('deleting');
-        const response = await axios.delete(`${serverBaseUrl}projects/`, {
-          params: { projectName },
+        const response = await axios.delete(`${url}projects/`, {
+          params: { projectName,userName },
         });
         setAddedBoard(listBoards[0]!==projectName?listBoards[0]:listBoards[1])
-      } catch (err) {
-        console.log(err);
+      } catch (error) {
+        console.log(error);if(error.message.includes('403')){alert('no promissions to delete')};
       }
     
   }
@@ -150,6 +148,161 @@ function AppProjects() {
       </DndProvider>
     </div>
   );
+}
+
+export default AppProjects;
+import React, { useEffect, useState } from 'react';
+import DivFilters from './components/filter-massions-dnd-func-data/ShowFiltersAndMassion';
+import BorderFilter from './components/borderFilter';
+import './App.css';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import axios from 'axios';
+import BoardOptions from './components/add board/boardOptions';
+import AddSprintModal from './components/add sprint/AddSprintModal';
+import SprintBoard from './components/add sprint/SprintBoard';
+
+function AppProjects() {
+    const [listBoards, setListBoards] = useState(null);
+    const [currentProject, setCurrentProject] = useState(null);
+    const [currentData, setCurrentData] = useState(null);
+    const [addedBoard, setAddedBoard] = useState(null);
+    const [isSprintActive, setIsSprintActive] = useState(false);
+    const [activeSprintDetails, setActiveSprintDetails] = useState(null);
+    const [isAddSprintModalOpen, setIsAddSprintModalOpen] = useState(false);
+    const [sprints, setSprints] = useState([]);
+    const [selectedBoard, setSelectedBoard] = useState(null);
+    const [selectedSprint, setSelectedSprint] = useState(null);
+
+
+    const serverBaseUrl = process.env.NODE_ENV === 'development' 
+        ? 'http://localhost:3000/' 
+        : 'https://project-jerusalem-2-server.vercel.app/';
+
+    useEffect(() => {
+        getDataBoards();
+    }, []);
+
+    const getDataBoards = async () => {
+        try {
+            const response = await axios.get(`${serverBaseUrl}projects/listofprojects`);
+            setListBoards(response.data);
+            if (response.data.length > 0) {
+                setCurrentProject(response.data[0]);
+                fetchProjectData(response.data[0]);
+            }
+        } catch (error) {
+            console.log('Error loading board list:', error);
+        }
+    };
+
+    const fetchProjectData = async (projectName) => {
+        try {
+            const response = await axios.get(`${serverBaseUrl}missions/${projectName}`);
+            setCurrentData(response.data);
+        } catch (error) {
+            console.error('Error fetching project data:', error);
+        }
+    };
+
+    const handleSprintAdded = (sprintData) => {
+      setActiveSprintDetails(sprintData);
+      setIsSprintActive(true);
+      setSprints(prevSprints => [...prevSprints, { boardName: currentProject, sprintName: sprintData.sprintName }]);
+    };
+
+    const handleItemSelection = async (selectedItem) => {
+        const [boardName, sprintName] = selectedItem.split("-");
+        setSelectedBoard(boardName);
+        setSelectedSprint(sprintName);
+    
+        if (sprintName) {
+            fetchSprintData(sprintName, boardName);
+        } else {
+            fetchBoardData(boardName);
+        }
+    };
+    
+    
+
+    
+    const fetchBoardData = async (boardName) => {
+        const url = `${serverBaseUrl}missions/${boardName}`;
+        console.log("Fetching data from:", url);
+        try {
+            const response = await axios.get(url);
+            console.log("Fetched Board Data:", response.data);
+            setCurrentData(response.data);
+            setIsSprintActive(false); // Deactivate sprint view
+            setActiveSprintDetails(null); // Reset sprint details
+        } catch (error) {
+            console.error('Error fetching board data:', error);
+        }
+    };
+    
+    const fetchSprintData = (sprintName, boardName) => {
+        const sprintData = sprints.find(sprint => sprint.sprintName === sprintName && sprint.boardName === boardName);
+        if (sprintData) {
+            setActiveSprintDetails(sprintData);
+            setIsSprintActive(true);
+            setCurrentData(sprintData.tasks); // Assuming sprintData.tasks is an array of tasks
+        } else {
+            console.error("Sprint data not found for:", sprintName);
+            // Handle the error case
+        }
+    };
+    
+      
+      useEffect(() => {
+        console.log("Current Project:", currentProject);
+        console.log("Current Data:", currentData);
+      }, [currentProject, currentData]);
+      
+    
+    const toggleAddSprintModal = () => {
+        setIsAddSprintModalOpen(!isAddSprintModalOpen);
+    };
+
+    if (currentData === null || listBoards === null) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <BorderFilter 
+                onProjectChange={handleItemSelection}
+                listProjects={listBoards}
+                listSprints={sprints}
+                newboard={addedBoard}
+            />
+            <BoardOptions 
+                addfunc={name => fetchProjectData(name)} 
+                editFunc={fetchProjectData} 
+                deleteBoardFunc={fetchProjectData} 
+                projectName={currentProject} 
+            />
+            {!isSprintActive && (
+                <button onClick={toggleAddSprintModal}>
+                    Add Sprint
+                </button>
+            )}
+            {isAddSprintModalOpen && (
+                <AddSprintModal
+                    isOpen={isAddSprintModalOpen}
+                    onClose={toggleAddSprintModal}
+                    onSprintAdded={handleSprintAdded}
+                    projectNames={currentData.map((project) => project)}
+                />
+            )}
+            <DndProvider backend={HTML5Backend}>
+                {!isSprintActive ? (
+                    <DivFilters projectData={currentData} collection={currentProject} />
+                ) : (
+                    <SprintBoard sprintDetails={activeSprintDetails} />
+                )}
+            </DndProvider>
+        </div>
+    );
 }
 
 export default AppProjects;
